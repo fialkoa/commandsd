374e8f3de3357d2208156fb966dc1297d5ffcb63
fialkoa <43009411+fialkoa@users.noreply.github.com>
2018-09-12 17:14:55 +0300

    Initial commit

---
+++ MyCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace commands.Commands
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Reset { get; set; }
+
+        static MyCommands()
+        {
+            Exit = new RoutedCommand("Exit", typeof(MyCommands));
+            Reset = new RoutedCommand("Reset", typeof(MyCommands));
+        }
+    }
+}
